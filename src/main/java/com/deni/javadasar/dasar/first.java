6779386e09d60732f256d76a63b568cb4d596962
denigunawan <artdengun@gmail.com>
2020-09-15 20:58:20 +0700

    first commit

--- src/main/java/com/deni/javadasar/dasar/first.java
+++ src/main/java/com/deni/javadasar/dasar/first.java
@@ -3,5 +3,6 @@ package com.deni.javadasar.dasar;
 public class first {
     public static void main(String[] args) {
         System.out.println("selamat datang di spring-boot");
+        System.out.println("ini latihan kedua ");
     }
 }
